2ed721690133521f7295e9435f734f9bdc14d036
coquicox <nicolasroy96@gmail.com>
2015-12-12 12:55:00 +0100

    Ajout du death trigger

---
+++ LudumDare/Assets/DeathTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider entering)
+    {
+        Destroy(entering.gameObject);
+    }
+}
