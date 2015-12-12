b6b9792d91b0f1eb819bde22bdc05edfbad02322
Jordan Bas <madjojo70@live.fr>
2015-12-12 12:49:25 +0100

    Tapis roulant de la win;

---
+++ LudumDare/Assets/ConveyorBelt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBelt : MonoBehaviour
+{
+
+    private float time;
+    public float spawnFrequence;
+    public Vector3 position;
+    public GameObject Objetproduit;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > spawnFrequence)
+        {
+            Instantiate(Objetproduit, position, Quaternion.identity);
+            time = 0;
+        }
+    }
+}
